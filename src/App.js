0feb55dca0e7ea90068a5765a3449770c6dcdea1
Andrew <andrewrusu87@hotmail.com>
2018-10-07 10:39:02 -0400

    Eject project, added css modules

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import classes from './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        
+      <div className={classes.App}>
       </div>
     );
   }
