a8cd8edf1f88f1a80461437badf6608b6608f843
Andrew <andrewrusu87@hotmail.com>
2018-10-12 06:32:26 -0400

    Added the crossword board from a mock puzzle with some click functionality

---
+++ src/components/Square/Square.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import classes from './Square.css';
+
+const square = (props) => {
+  const squareType = props.type;
+  let squareCSS = [classes.white];
+  let squareNumber = null;
+  let clicked = props.clicked;
+
+  if (isNaN(squareType)) {
+    if (squareType === 'B') {
+      squareCSS = [...squareCSS, classes.black];
+      clicked = null;
+    }
+  }
+  else {
+    squareCSS = [...squareCSS, classes.numbered]
+    squareNumber = +squareType;
+  }
+
+  if (props.focused) {
+    squareCSS = [...squareCSS, classes.focused];
+  }
+
+  return (
+    <div className={squareCSS.join(' ')} onClick={clicked}>{squareNumber}</div>
+  );
+}
+
+export default square;
---
+++ src/containers/Grid/Grid.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+
+import Square from '../../components/Square/Square';
+
+import { MockPuzzle } from '../../MockPuzzle/MockPuzzle';
+
+import classes from './Grid.css';
+
+class Grid extends Component {
+  state = {
+    gridValues: null
+  }
+
+  componentDidMount () {
+    const grid = new Array(15);
+
+    for (let i = 0; i < grid.length; i++) {
+      grid[i] = new Array(15);
+      for (let j = 0; j < grid[i].length; j++) {
+        grid[i][j] = {focus: false};
+      }
+    }
+
+    this.setState({gridValues: grid})
+  }
+
+  squareClickedHandler = (row, col) => {
+    const grid = new Array(15);
+
+    for (let i = 0; i < grid.length; i++) {
+      grid[i] = new Array(15);
+      for (let j = 0; j < grid[i].length; j++) {
+        if (i === row && j === col) {
+          grid[i][j] = {focus: true};
+        } else {
+          grid[i][j] = {focus: false};
+        }
+      }
+    }
+
+    this.setState({gridValues: grid})
+  }
+
+  render () {
+    let squares = null;
+    let key = 0;
+
+    if (this.state.gridValues) {
+      squares = [];
+      for (let i = 0; i < MockPuzzle.length; i++) {
+        for (let j = 0; j < MockPuzzle[i].length; j++) {
+          squares.push(
+            <Square key={key++} 
+              focused={this.state.gridValues[i][j].focus} 
+              type={MockPuzzle[i][j]} 
+              clicked={() => this.squareClickedHandler(i, j)} />);
+        }
+      }
+    }
+    
+    return (
+      <div className={classes.Grid}>
+        {squares}
+      </div>
+    );
+  }
+}
+
+export default Grid;
